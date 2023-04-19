7cc11eb96e61cd1fec038195a1a00d7987d2be09
shiraAx <shira89516@gmail.com>
2023-04-19 23:26:25 +0300

    my

--- NewToDoApi/Program.cs
+++ NewToDoApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("OpenPolicy",
                           policy =>
                           {
-                              policy.WithOrigins("http://localhost:3000")
+                              policy.WithOrigins("https://practycode1.onrender.com")
                                                   .AllowAnyHeader()
                                                   .AllowAnyMethod();
                           });
