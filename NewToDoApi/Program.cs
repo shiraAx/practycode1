5495f9fc104b893cbc131897a3bdfaa89878306e
shiraAx <shira89516@gmail.com>
2023-04-19 23:34:38 +0300

    my

--- NewToDoApi/Program.cs
+++ NewToDoApi/Program.cs
@@ -1,7 +1,103 @@
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.OpenApi.Models;
+// using NewToDoApi;
+
+
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("OpenPolicy",
+//                           policy =>
+//                           {
+//                               policy.WithOrigins("https://practycode1.onrender.com")
+//                                                   .AllowAnyHeader()
+//                                                   .AllowAnyMethod();
+//                           });
+// });
+
+
+
+// builder.Services.AddSwaggerGen(c =>
+// {
+//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+// });
+// builder.Services.AddDbContext<ToDoDbContext>();
+// var app = builder.Build();
+
+// app.UseCors("OpenPolicy");
+// app.UseSwagger();
+// app.UseSwaggerUI(c =>
+// {
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+// });
+
+
+// app.MapGet("/",  () =>"hellow");
+// app.MapGet("/items", async (ToDoDbContext db) =>
+//     await db.Items.ToListAsync());
+
+// // POST /todoitems
+// app.MapPost("/todoitems", async (Item Dto, ToDoDbContext Db) =>
+// {
+//     // var todoItem = new Item
+//     // {
+//     //     IsComplete = Dto.IsComplete,
+//     //     Name = Dto.Name
+//     // };
+
+//     Db.Items.Add(Dto);
+//     await Db.SaveChangesAsync();
+//     return Results.Created($"/todoitems/{Dto.IdItems}",  Dto);
+// });
+
+// app.MapPut("/todoitems/{id}", async (int Id, string name,bool IsComplete, ToDoDbContext Db) =>
+// {
+//     var todo = await Db.Items.FindAsync(Id);
+//      Console.WriteLine("IsComplete",IsComplete);
+
+//     if (todo is null) return Results.NotFound();
+
+//     todo.Name = name;
+//     todo.IsComplete = IsComplete;
+
+//     await Db.SaveChangesAsync();
+
+//     return Results.NoContent();
+// });
+
+
+
+// app.MapDelete("/todoitems/{id}", async (int Id, ToDoDbContext Db) =>
+// {
+//     if (await Db.Items.FindAsync(Id) is Item todo)
+//     {
+//         Db.Items.Remove(todo);
+//         await Db.SaveChangesAsync();
+//         return Results.Ok(todo);
+//     }
+
+//     return Results.NotFound();
+// });
+
+
+// app.MapMethods("/options-or-head", new[] { "OPTIONS", "HEAD" }, 
+//                           () => "This is an options or head request ");
+
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine($"Incoming request: {context.Request.Method} {context.Request.Path}");
+//     await next.Invoke();
+// });
+
+
+// app.Run();
+
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using NewToDoApi;
-
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -9,19 +105,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("OpenPolicy",
-                          policy =>
-                          {
-                              policy.WithOrigins("https://practycode1.onrender.com")
-                                                  .AllowAnyHeader()
-                                                  .AllowAnyMethod();
-                          });
+                      policy =>
+                      {
+                          policy.WithOrigins("https://practycode1.onrender.com")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowCredentials(); // Added AllowCredentials
+                      });
 });
-
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("AllowOrigin",
-//         builder => builder.WithOrigins("https://practycode1.onrender.com"));
-// });
 
 builder.Services.AddSwaggerGen(c =>
 {
@@ -30,36 +121,30 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddDbContext<ToDoDbContext>();
 var app = builder.Build();
 
+app.UseRouting(); // Added UseRouting
 app.UseCors("OpenPolicy");
+app.UseAuthorization(); // Added UseAuthorization
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
 });
 
-
-app.MapGet("/",  () =>"hellow");
+app.MapGet("/", () => "Hello, World!");
 app.MapGet("/items", async (ToDoDbContext db) =>
     await db.Items.ToListAsync());
 
-// POST /todoitems
 app.MapPost("/todoitems", async (Item Dto, ToDoDbContext Db) =>
 {
-    // var todoItem = new Item
-    // {
-    //     IsComplete = Dto.IsComplete,
-    //     Name = Dto.Name
-    // };
-
     Db.Items.Add(Dto);
     await Db.SaveChangesAsync();
-    return Results.Created($"/todoitems/{Dto.IdItems}",  Dto);
+    return Results.Created($"/todoitems/{Dto.IdItems}", Dto);
 });
 
-app.MapPut("/todoitems/{id}", async (int Id, string name,bool IsComplete, ToDoDbContext Db) =>
+app.MapPut("/todoitems/{id}", async (int Id, string name, bool IsComplete, ToDoDbContext Db) =>
 {
     var todo = await Db.Items.FindAsync(Id);
-     Console.WriteLine("IsComplete",IsComplete);
+    Console.WriteLine("IsComplete", IsComplete);
 
     if (todo is null) return Results.NotFound();
 
@@ -70,8 +155,6 @@ app.MapPut("/todoitems/{id}", async (int Id, string name,bool IsComplete, ToDoDb
 
     return Results.NoContent();
 });
-
-
 
 app.MapDelete("/todoitems/{id}", async (int Id, ToDoDbContext Db) =>
 {
@@ -85,15 +168,13 @@ app.MapDelete("/todoitems/{id}", async (int Id, ToDoDbContext Db) =>
     return Results.NotFound();
 });
 
-
-app.MapMethods("/options-or-head", new[] { "OPTIONS", "HEAD" }, 
-                          () => "This is an options or head request ");
+app.MapMethods("/options-or-head", new[] { "OPTIONS", "HEAD" },
+    () => "This is an options or head request ");
 
 app.Use(async (context, next) =>
 {
     Console.WriteLine($"Incoming request: {context.Request.Method} {context.Request.Path}");
     await next.Invoke();
 });
-
 
 app.Run();
